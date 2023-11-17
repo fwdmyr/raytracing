733779ab1f1ae1863679f09d770c381a432fea3e
felix <felix.widmayer@rwth-aachen.de>
2023-11-17 06:45:35 +0100

    Final render

--- src/main.rs
+++ src/main.rs
@@ -8,15 +8,17 @@ use crate::math::hittable::*;
 use crate::math::sphere::*;
 use crate::math::vec3::*;
 
+use rand::Rng;
+
 fn main() -> Result<(), Box<(dyn std::error::Error + 'static)>> {
     const ASPECT_RATIO: f32 = 16.0 / 9.0;
-    const IMAGE_WIDTH: u32 = 400;
+    const IMAGE_WIDTH: u32 = 2180;
     const FOCAL_LENGTH: f32 = 1.0;
     const VERTICAL_FOV: f32 = 20.0;
-    const SAMPLES_PER_PIXEL: u32 = 100;
-    const MAX_RAY_BOUNCES: u32 = 10;
-    const DEFOCUS_ANGLE: f32 = 10.0;
-    const FOCUS_DIST: f32 = 3.4;
+    const SAMPLES_PER_PIXEL: u32 = 500;
+    const MAX_RAY_BOUNCES: u32 = 50;
+    const DEFOCUS_ANGLE: f32 = 0.6;
+    const FOCUS_DIST: f32 = 10.0;
 
     let params = CameraParameters::new(
         ASPECT_RATIO,
@@ -31,8 +33,8 @@ fn main() -> Result<(), Box<(dyn std::error::Error + 'static)>> {
 
     let mut camera = Camera::new(params);
 
-    let lookfrom = Vec3::new(-2.0, 2.0, 1.0);
-    let lookat = Vec3::new(0.0, 0.0, -1.0);
+    let lookfrom = Vec3::new(13.0, 2.0, 3.0);
+    let lookat = Vec3::new(0.0, 0.0, 0.0);
     let vup = Vec3::new(0.0, 1.0, 0.0);
     camera.set_frame(lookfrom, lookat, vup);
 
@@ -40,36 +42,55 @@ fn main() -> Result<(), Box<(dyn std::error::Error + 'static)>> {
 
     let mut world = HittableList::new();
 
-    let ground = Material::Lambertian(Pixel::from(&Vec3::new(0.0, 0.6, 0.6)));
-    let center = Material::Lambertian(Pixel::from(&Vec3::new(0.1, 0.2, 0.5)));
-    let left = Material::Dielectric(1.5);
-    let right = Material::Metal(Pixel::from(&Vec3::new(0.8, 0.8, 0.8)), 0.0);
+    let ground_material = Material::Lambertian(Pixel::from(&Vec3::new(0.5, 0.5, 0.5)));
+    world.push(Box::new(Sphere::new(
+        Vec3::new(0.0, -1000.0, 0.0),
+        1000.0,
+        ground_material,
+    )));
 
-    world.push(Box::new(Sphere::new(
-        Vec3::new(0.0, -100.5, -1.0),
-        100.0,
-        ground,
-    )));
-    world.push(Box::new(Sphere::new(
-        Vec3::new(0.0, 0.0, -1.0),
-        0.45,
-        center,
-    )));
-    world.push(Box::new(Sphere::new(
-        Vec3::new(-1.0, 0.0, -1.0),
-        0.45,
-        left,
-    )));
-    world.push(Box::new(Sphere::new(
-        Vec3::new(-1.0, 0.0, -1.0),
-        -0.45,
-        left,
-    )));
-    world.push(Box::new(Sphere::new(
-        Vec3::new(1.0, 0.0, -1.0),
-        0.45,
-        right,
-    )));
+    let mut rng = rand::thread_rng();
+
+    (-11..11).into_iter().for_each(|a| {
+        (-11..11).into_iter().for_each(|b| {
+            let choose_material = rng.gen::<f32>();
+            let center = Vec3::new(
+                a as f32 + 0.9 * rng.gen::<f32>(),
+                0.2,
+                b as f32 + 0.9 * rng.gen::<f32>(),
+            );
+
+            if (center - Vec3::new(4.0, 0.2, 0.0)).norm() > 0.9 {
+                match choose_material {
+                    x if (0.0..0.8).contains(&x) => {
+                        let albedo =
+                            Pixel::from(&(Vec3::random(0.0, 1.0) * Vec3::random(0.0, 1.0)));
+                        let material = Material::Lambertian(albedo);
+                        world.push(Box::new(Sphere::new(center, 0.2, material)));
+                    }
+                    x if (0.8..0.95).contains(&x) => {
+                        let albedo = Pixel::from(&Vec3::random(0.5, 1.0));
+                        let fuzz = rng.gen_range(0.0..0.5);
+                        let material = Material::Metal(albedo, fuzz);
+                        world.push(Box::new(Sphere::new(center, 0.2, material)));
+                    }
+                    _ => {
+                        let material = Material::Dielectric(1.5);
+                        world.push(Box::new(Sphere::new(center, 0.2, material)));
+                    }
+                }
+            }
+        });
+    });
+
+    let material = Material::Dielectric(1.5);
+    world.push(Box::new(Sphere::new(Vec3::new(0.0, 1.0, 0.0), 1.0, material)));
+
+    let material = Material::Lambertian(Pixel::from(&Vec3::new(0.4, 0.2, 0.1)));
+    world.push(Box::new(Sphere::new(Vec3::new(-4.0, 1.0, 0.0), 1.0, material)));
+
+    let material = Material::Metal(Pixel::from(&Vec3::new(0.7, 0.6, 0.5)), 0.0);
+    world.push(Box::new(Sphere::new(Vec3::new(4.0, 1.0, 0.0), 1.0, material)));
 
     camera.render(path, &world);
 
